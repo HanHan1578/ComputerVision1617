dea626eace5daaf7536411149987b76d853a3c43
HanHan1578 <happy810705@gmail.com>
2016-11-11 13:15:19 +0100

    q4 revision

--- q4_gaussian_filtering/gaussianfilter.m
+++ q4_gaussian_filtering/gaussianfilter.m
@@ -4,9 +4,32 @@ function outimg = gaussianfilter(inimg, sigma)
     window = 2*round(3*sigma)+1;
     outimg = zeros(w,h);
     gaussian = gauss(-3*sigma:3*sigma, sigma);
-    outimg_tmp = conv2(inimg, gaussian, 'same'); 
-    gaussian = gauss(-3*sigma:3*sigma, sigma);
-    outimg = conv2(outimg_tmp, gaussian', 'same'); 
+%     outimg_tmp = conv2(inimg, gaussian, 'same');
+%     gaussian = gauss(-3*sigma:3*sigma, sigma);
+%     outimg = conv2(outimg_tmp, gaussian', 'same'); 
+outimg_tmp = zeros(w+2*round(3*sigma),h+2*round(3*sigma));
+outimg_tmp2 = zeros(w+2*round(3*sigma),h+2*round(3*sigma));
+outimg_tmp3 = zeros(w+2*round(3*sigma),h+2*round(3*sigma));
+    for i = 1:w
+        for j = 1:h
+            outimg_tmp(i+round(3*sigma)-1,j+round(3*sigma)-1) = inimg(i, j);
+        end
+    end
+    for i = round(3*sigma)+1:w+round(3*sigma)
+        for j = round(3*sigma)+1:h+round(3*sigma)
+            outimg_tmp2(i,j) = sum(outimg_tmp(i, j-round(3*sigma):j+round(3*sigma)).*gaussian);
+        end
+    end
+    for i = round(3*sigma)+1:w+round(3*sigma)
+        for j = round(3*sigma)+1:h+round(3*sigma)
+            outimg_tmp3(i,j) = sum(outimg_tmp2(i-round(3*sigma):i+round(3*sigma), j).*gaussian');
+        end
+    end
+    for i = 1:w
+        for j = 1:h
+            outimg(i,j) = outimg_tmp3(i+round(3*sigma),j+round(3*sigma));
+        end
+    end
 %     for j = 1:w
 %         for i = 1:h-window+1
 %             gaussian = gauss(-round(3*sigma):round(3*sigma), sigma);
